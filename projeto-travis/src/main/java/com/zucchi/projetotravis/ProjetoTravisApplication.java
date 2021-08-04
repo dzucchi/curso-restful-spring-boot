060b12c2b8f78b18d4ca8a5f22d4bdc3d0afc19c
Diogo Zucchi <diogo_zucchi@terceiros.sicredi.com.br>
2021-08-04 11:05:07 -0300

    Start inicial do projeto com Travis e adicionando o status do build.

---
+++ projeto-travis/src/main/java/com/zucchi/projetotravis/ProjetoTravisApplication.java
@@ -0,0 +1,13 @@
+package com.zucchi.projetotravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTravisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTravisApplication.class, args);
+	}
+
+}
---
+++ projeto-travis/src/test/java/com/zucchi/projetotravis/ProjetoTravisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zucchi.projetotravis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTravisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
